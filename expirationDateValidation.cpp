9bf789714fe75576b86c4372469054198351d76b
Kerem Yaldız <keremyaldiz@gmail.com>
2017-01-28 22:18:45 +0300

    remove parts which are not neccesary

--- expirationDateValidation.cpp
+++ expirationDateValidation.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <regex>
-#include <iterator>
 
 bool expirationDateValidation(std::string & word){
   std::regex format("^(([0-2]?[0-9])|(3[0-1]))(.|:|/|-)((0?[0-9])|(1[0-2]))(.|:|/|-)((201[7-9])|(202[0-9]))$");
@@ -13,8 +12,6 @@ int main(int argc, char const *argv[]) {
   std::string word;
   std::cout<<"Enter an expiration date> ";
   std::getline(std::cin,word);
-  std::string::iterator end_pos = std::remove(word.begin(), word.end(), ' ');
-  word.erase(end_pos, word.end());
 
   if(expirationDateValidation(word)){
     std::cout << word << " Valid" << std::endl;
